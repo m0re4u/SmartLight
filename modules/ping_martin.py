0e8bd48b0fc48016361281ff15c601001847a03a
Adriaan de Vries <adriaan.de.vries@hotmail.com>
2017-02-07 15:01:39 +0100

    Pinging. Also True/False instead of 1/0

---
+++ modules/ping_martin.py
@@ -0,0 +1,17 @@
+from os import system as command
+from platform import system as os_name
+
+def light_on(address='145.109.25.144'):
+
+
+    # this line largely taken from http://stackoverflow.com/questions/2953462/pinging-servers-in-python
+    parameters = '-n 1 ' if os_name().lower()=='windows' else '-c 1 '
+
+    null_outstream = ' > NUL' if os_name().lower()=='windows' else ' > \\dev\\null'
+    
+    result = command('ping ' + parameters + address + null_outstream)
+    return not result
+
+
+if __name__ == '__main__':
+    print(light_on())
