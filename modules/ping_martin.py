3059229fbe361bfae8001e3d3b3fdff962e2e626
Michiel van der Meer <michielmeer@live.nl>
2017-02-07 15:11:29 +0100

    added ping to config

--- modules/ping_martin.py
+++ modules/ping_martin.py
@@ -1,15 +1,20 @@
+import yaml
 from os import system as command
 from platform import system as os_name
 
-def light_on(address='145.109.25.144'):
 
+def light_on(yml_path="../config.yml"):
+    with open(yml_path) as f:
+        config = yaml.load(f)
+    # this line largely taken from
+    # http://stackoverflow.com/questions/2953462/pinging-servers-in-python
+    parameters = '-n 1 ' if os_name().lower() == 'windows' else '-c 1 '
 
-    # this line largely taken from http://stackoverflow.com/questions/2953462/pinging-servers-in-python
-    parameters = '-n 1 ' if os_name().lower()=='windows' else '-c 1 '
+    null_outstream = ' > NUL' if os_name().lower() == 'windows'\
+        else ' > /dev/null'
 
-    null_outstream = ' > NUL' if os_name().lower()=='windows' else ' > \\dev\\null'
-    
-    result = command('ping ' + parameters + address + null_outstream)
+    result = command(
+        'ping ' + parameters + config['ping_address'] + null_outstream)
     return not result
 
 
