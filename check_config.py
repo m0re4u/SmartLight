f12ba1b5979e50d1773f74d8b22529757662043f
Adriaan de Vries <adriaan.de.vries@hotmail.com>
2017-03-10 14:13:04 +0100

    Changed config file to useful syntax for controlling multiple lights, and added an automatic check for which lights are still available, and which are controlled by more than one module

---
+++ check_config.py
@@ -0,0 +1,32 @@
+import yaml
+
+def fix(pos, size, grid):
+    for i in range(pos[0], pos[0]+size[0]):
+        for j in range(pos[1], pos[1]+size[1]):
+            if grid[j][i] == '0':
+                grid[j][i] = 'X'
+            else:
+                grid[j][i] = '0'
+
+
+
+print ("Available lights are indicated by '.', unavailable lights are indicated by '0'\n\n"
+       + "lights directed by multiple modules are indicated with 'X'; fix those")
+
+grid = [['.'] * 32 for _ in range(16)]
+
+with open('./config.yml') as f:
+    config = yaml.load(f)
+for module in config['modules']:
+    try:
+        pos = config['modules'][module]['pos']
+        size = config['modules'][module]['size']
+        fix(pos, size, grid)
+    except TypeError:
+        for pair in config['modules'][module]:
+            pos = pair['pos']
+            size = pair['size']
+            fix(pos, size, grid)
+            
+for line in grid:
+    print(' '.join(line))
